c6504b4b8c7d130b672a28086cf99c2f95aad33a
unknown <stk4zn@RH340-06.lab.cs.virginia.edu>
2014-04-03 20:19:59 -0400

    added interactables

---
+++ GameName1/GameName1/Food.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameName1
+{
+    class Food: PickUp
+    {
+
+        public Food(Seizonsha game)
+            : base(game, Static.PIXEL_THIN, 40, 40)
+        {
+            this.tint = Color.Brown;
+        }
+        public override void Interact(Player player)
+        {
+            setRemove(true);
+            game.healEntity(null, player, 100, Static.DAMAGE_TYPE_ALL);
+        }
+
+        public override string Message(Player player)
+        {
+            return "EAT FOOD";
+        }
+
+        public override bool Available(Player player)
+        {
+            return true;
+        }
+
+        protected override void OnDie()
+        {
+            
+        }
+
+        public override void OnSpawn()
+        {
+        }
+
+        public override void collideWithWall()
+        {
+        }
+
+        public override void collide(GameEntity entity)
+        {
+        }
+
+        public override string getName()
+        {
+            return "FOOD";
+        }
+    }
+}
---
+++ GameName1/GameName1/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using GameName1.Effects;
+using GameName1.Interfaces;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameName1
+{
+    abstract class PickUp : GameEntity, Interactable
+    {
+
+
+        public PickUp(Seizonsha game, Texture2D sprite, int width, int height)
+            : base(game, sprite, width, height, Static.TARGET_TYPE_NOT_DAMAGEABLE, 0)
+        {
+
+        }
+
+        public abstract void Interact(Player player);
+
+
+        public abstract string Message(Player player);
+
+        public abstract bool Available(Player player);
+
+    }
+}
