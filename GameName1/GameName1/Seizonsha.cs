53e5df1d62847deb5683b354d260936b8df4e445
ekt7be <ekt7be@virginia.edu>
2014-04-18 06:13:12 -0400

    made game sound louder

--- GameName1/GameName1/Seizonsha.cs
+++ GameName1/GameName1/Seizonsha.cs
@@ -206,7 +206,7 @@ namespace GameName1
 
             gameSoundLoop = gameSound.CreateInstance();
             gameSoundLoop.IsLooped = true;
-            gameSoundLoop.Volume = .1f;
+            gameSoundLoop.Volume = .5f;
             bossSound = Content.Load<SoundEffect>("sound/armageddon");
             bossSoundLoop = bossSound.CreateInstance();
             bossSoundLoop.IsLooped = true;
